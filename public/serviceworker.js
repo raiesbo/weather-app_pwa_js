f0ef777317bbcc51a8ad75a888026f8f8bc0912e
raiesbo <raiesbo8@gmail.com>
2020-10-18 17:37:04 +0200

    finished functionality, missing styles

---
+++ public/serviceworker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "version-1"
+const urlsToCache = [ 'index.html', 'offline.html' ];
+
+const self = this;
+
+// INSTALL SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+// LISTEN FOR REQUESTS
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() =>caches.match('offline.html'))
+            })
+    )
+
+});
+
+// ACTIVATE SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+});
